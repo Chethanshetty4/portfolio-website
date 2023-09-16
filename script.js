0565cf0a90b96269ae540495dd60ba26df79e19a
Chethanshetty4 <118925283+Chethanshetty4@users.noreply.github.com>
2023-09-16 21:46:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ let menu = document.querySelector('#menu-icon');
 let menulist = document.querySelector('.menulist');
 
 menu.addEventListener('click', ()=>{
-	menu.classList.toggle('dummycode');
 	menulist.classList.toggle('open');
 })
